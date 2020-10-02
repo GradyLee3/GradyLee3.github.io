e45679e4c7a8d71ccb387c3ac2922c48c7f94858
unknown <GradyLee3>
2020-10-02 22:09:10 +0800

    Site updated: 2020-10-02 22:09:06

--- js/background.js
+++ js/background.js
@@ -3,3 +3,7 @@ var full_page = document.getElementsByClassName("full_page");
 if (full_page.length != 0) {
   full_page[0].style.background = "transparent";
 }
+// 设置页脚博主会动的心
+$(document).ready(function(e){
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> HEAVEN');
+})
